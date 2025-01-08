a9d34e793f86a527de45cd0f563bff89a3c9a966
Kelvin Sidharta Sie <c14210307@john.petra.ac.id>
2025-01-08 13:01:44 +0700

    feat: admin delete a room and kick all users

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -2,8 +2,10 @@ import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 import { Link, useNavigate } from "react-router-dom";
 import api from "../axios/axios";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
+import { io } from "socket.io-client";
+
+const websocket = "http://localhost:5174";
+const socket = io.connect(websocket);
 
 const AdminHome = () => {
   const { user, token } = useContext(AppContext);
@@ -61,6 +63,40 @@ const AdminHome = () => {
     }
   }
 
+  function handleDeleteRoom(roomId) {
+    return async (e) => {
+      e.preventDefault();
+      try {
+        // Fetch users in the room
+        const usersResponse = await api.get(`room/${roomId}/users`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const userIds = usersResponse.data.map((user) => user.id);
+
+        // Delete the room
+        const response = await api.delete(`room/${roomId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          setRooms((prevRooms) => prevRooms.filter((room) => room.id !== roomId));
+          console.log("Room deleted:", roomId);
+
+          userIds.forEach((userId) => {
+            socket.emit("user_kicked", userId);
+          });
+        }
+      } catch (error) {
+        console.error("Error deleting room:", error);
+      }
+    };
+  }
+
   function handleChange(e) {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -70,42 +106,42 @@ const AdminHome = () => {
   }
 
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1">
-        <Navbar />
-        <div className="p-6 bg-gray-100 min-h-screen">
-          <div className="p-6 bg-white rounded shadow-md">
-            <h2 className="mb-4 text-2xl font-bold text-center">Admin Home</h2>
-            <p>Welcome {user && user.email}</p>
-            <form onSubmit={createRoom} className="mt-4">
-              <input type="text" name="id" placeholder="Room ID" value={formData.id} onChange={handleChange} className="w-full p-2 mb-4 border rounded" />
-              <input type="text" name="name" placeholder="Room Name" value={formData.name} onChange={handleChange} className="w-full p-2 mb-4 border rounded" />
-              <input type="text" name="description" placeholder="Room Description" value={formData.description} onChange={handleChange} className="w-full p-2 mb-4 border rounded" />
-              <button type="submit" className="w-full p-2 text-white bg-blue-500 rounded">
-                Create Room
-              </button>
-            </form>
-            <div className="mt-6">
-              <h3 className="mb-4 text-xl font-bold text-center">Rooms</h3>
-              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                {rooms.map((room) => (
-                  <div key={room.id} className="p-4 bg-white rounded shadow-md">
-                    <h4 className="mb-2 text-lg font-bold">
-                      {room.id} - {room.name}
-                    </h4>
-                    <p>{room.description}</p>
-                    <Link to={`/room/${room.id}`} className="mt-4 rounded-lg bg-gray-100 text-center">
-                      View
-                    </Link>
-                  </div>
-                ))}
+    <>
+      <h2 className="mb-4 text-2xl font-bold text-center">Admin Home</h2>
+      <p>Welcome {user && user.email}</p>
+      <form onSubmit={createRoom} className="mt-4">
+        <input type="text" name="id" placeholder="Room ID" value={formData.id} onChange={handleChange} className="w-full p-2 mb-4 border rounded" />
+        <input type="text" name="name" placeholder="Room Name" value={formData.name} onChange={handleChange} className="w-full p-2 mb-4 border rounded" />
+        <input type="text" name="description" placeholder="Room Description" value={formData.description} onChange={handleChange} className="w-full p-2 mb-4 border rounded" />
+        <button type="submit" className="w-full p-2 text-white bg-blue-500 rounded">
+          Create Room
+        </button>
+      </form>
+      <div className="mt-6">
+        <h3 className="mb-4 text-xl font-bold text-center">Rooms</h3>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {rooms.map((room) => (
+            <div key={room.id} className="p-4 bg-white rounded shadow-md">
+              <h4 className="mb-2 text-lg font-bold">
+                {room.id} - {room.name}
+              </h4>
+              <p>{room.description}</p>
+              <div className="flex space-x-2">
+                <Link to={`/room/${room.id}`} className="mt-4 block p-2 text-center text-white bg-blue-500 rounded">
+                  View
+                </Link>
+
+                <form action="" onSubmit={handleDeleteRoom(room.id)}>
+                  <button className="mt-4 block p-2 text-center text-white bg-red-500 rounded" type="submit">
+                    Delete
+                  </button>
+                </form>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
