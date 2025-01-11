34010951aed764395e6b18d127858d41c315b7e7
Kelvin Sidharta Sie <c14210307@john.petra.ac.id>
2025-01-11 21:50:57 +0700

    feat: improved logic for drag and drop

--- src/pages/Simulation2.jsx
+++ src/pages/Simulation2.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
-import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
+import { DndContext, DragOverlay, useDraggable, useDroppable } from "@dnd-kit/core";
 import api from "./../axios/axios";
 import { CSS } from "@dnd-kit/utilities";
 
-const DraggableContainer = ({ id, text, style }) => {
+const DraggableContainer = ({ id, text, style, isDragging }) => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id,
   });
 
   const defaultStyle = {
     transform: CSS.Transform.toString(transform),
+    zIndex: isDragging ? 9999 : "auto",
   };
 
   return (
-    <div ref={setNodeRef} style={{ ...defaultStyle, ...style }} {...listeners} {...attributes} className="p-2 m-1 bg-blue-400 text-white rounded cursor-move shadow">
+    <div ref={setNodeRef} style={{ ...defaultStyle, ...style }} {...listeners} {...attributes} className={`p-2 m-1 bg-blue-400 text-white rounded cursor-move shadow ${isDragging ? "dragging" : ""}`}>
       {text}
     </div>
   );
@@ -40,7 +41,7 @@ const DroppableCell = ({ id, children, coordinates }) => {
 const ContainerBay = ({ id, rows, columns, children }) => {
   return (
     <div
-      className="grid gap-1 m-2 border border-gray-400"
+      className="grid gap-1 m-2 border border-gray-400 rounded shadow-sm"
       style={{
         gridTemplateColumns: `repeat(${columns}, 1fr)`,
         gridTemplateRows: `repeat(${rows}, 1fr)`,
@@ -60,25 +61,69 @@ const Simulation2 = () => {
     { id: "box1", area: "docks-0" },
     { id: "box2", area: "docks-1" },
     { id: "box3", area: "docks-2" },
+    { id: "box4", area: "docks-3" },
+    { id: "box5", area: "docks-4" },
+    { id: "box6", area: "docks-5" },
+    { id: "box7", area: "docks-6" },
+    { id: "box8", area: "docks-7" },
+    { id: "box9", area: "docks-8" },
+    { id: "box10", area: "docks-9" },
+    { id: "box11", area: "docks-10" },
+    { id: "box12", area: "docks-11" },
+    { id: "box13", area: "docks-12" },
+    { id: "box14", area: "docks-13" },
+    { id: "box15", area: "docks-14" },
+    { id: "box16", area: "docks-15" },
+    { id: "box17", area: "docks-16" },
+    { id: "box18", area: "docks-17" },
+    { id: "box19", area: "docks-18" },
+    { id: "box20", area: "docks-19" },
+    { id: "box21", area: "docks-20" },
+    { id: "box22", area: "docks-21" },
+    { id: "box23", area: "docks-22" },
+    { id: "box24", area: "docks-23" },
+    { id: "box25", area: "docks-24" },
+    { id: "box26", area: "docks-25" },
+    { id: "box27", area: "docks-26" },
+    { id: "box28", area: "docks-27" },
+    { id: "box29", area: "docks-28" },
+    { id: "box30", area: "docks-29" },
   ]);
 
-  const [baySize, setBaySize] = useState({ rows: 3, columns: 3 });
+  const [baySize, setBaySize] = useState({ rows: 2, columns: 2 });
   const [bayCount, setBayCount] = useState(2);
   const [bayData, setBayData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [draggingItem, setDraggingItem] = useState(null); // State to track the dragging item
+  const itemsPerPage = 15;
+
+  const handleDragStart = (event) => {
+    setDraggingItem(event.active.id);
+  };
 
   const handleDragEnd = async (event) => {
     const { active, over } = event;
-    if (over && active.id !== over.id) {
+    setDraggingItem(null); // Reset dragging item
+    console.log("Active:", active);
+    console.log("Over:", over);
+
+    if (over) {
+      const activeItem = droppedItems.find((item) => item.id === active.id);
+      if (activeItem && activeItem.area === over.id) {
+        console.log("Item dropped in the same cell, no API call needed");
+        return;
+      }
+
       setDroppedItems((prevItems) => prevItems.map((item) => (item.id === active.id ? { ...item, area: over.id } : item)));
       console.log(`Dropped item: ${active.id} into ${over.id}`);
 
       try {
-        await api.get("/moveDragMe", {
+        const res = await api.get("/moveDragMe", {
           // itemId: active.id,
-          // from: droppedArea,
+          // from: activeItem.area,
           // to: over.id,
         });
-        console.log("API call successful");
+        console.log("API call successful", res.data);
       } catch (error) {
         console.error("API call failed", error);
       }
@@ -92,6 +137,10 @@ const Simulation2 = () => {
 
   const handleBayCountChange = (e) => {
     setBayCount(parseInt(e.target.value));
+  };
+
+  const handlePageChange = (direction) => {
+    setCurrentPage((prevPage) => prevPage + direction);
   };
 
   useEffect(() => {
@@ -108,9 +157,12 @@ const Simulation2 = () => {
     console.log("Bay Data:", newBayData);
   }, [droppedItems, baySize, bayCount]);
 
+  const paginatedItems = droppedItems.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
+
   return (
     <>
-      <div className="p-5 border-b border-gray-400">
+      {/* Configuration Form */}
+      <div className="p-5 border-b border-gray-400 w-screen">
         <h4 className="text-lg font-semibold mb-4">Configure Ship</h4>
         <div className="flex mb-4">
           <div className="mr-4">
@@ -123,48 +175,65 @@ const Simulation2 = () => {
           </div>
           <div>
             <label className="block mb-2">Number of Bays:</label>
-            <input max="10" type="number" value={bayCount} onChange={handleBayCountChange} min="1" className="w-12 p-1 border border-gray-300 rounded" />
+            <input max="8" type="number" value={bayCount} onChange={handleBayCountChange} min="1" className="w-12 p-1 border border-gray-300 rounded" />
           </div>
         </div>
       </div>
-      <DndContext onDragEnd={handleDragEnd}>
-        <div className="flex flex-col flex-1 overflow-x-auto overflow-y-hidden p-5 h-screen w-screen">
-          <h4 className="text-lg font-semibold mb-4">Ship</h4>
-          <div className="flex gap-5 overflow-x-auto">
-            {Array.from({ length: bayCount }).map((_, bayIndex) => (
-              <div key={`bay-${bayIndex}`}>
-                <h5 className="text-center text-md font-medium mb-2">Bay {bayIndex + 1}</h5>
-                <ContainerBay id={`bay-${bayIndex}`} rows={baySize.rows} columns={baySize.columns}>
-                  {Array.from({ length: baySize.rows * baySize.columns }).map((_, cellIndex) => {
-                    const rowIndex = Math.floor(cellIndex / baySize.columns);
-                    const colIndex = cellIndex % baySize.columns;
-                    const coordinates = `${bayIndex + 1}${rowIndex}${colIndex}`;
-                    return (
-                      <DroppableCell key={`bay-${bayIndex}-${cellIndex}`} id={`bay-${bayIndex}-${cellIndex}`} coordinates={coordinates}>
-                        {droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`) && (
-                          <DraggableContainer id={droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`).id} text={droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`).id} />
-                        )}
-                      </DroppableCell>
-                    );
-                  })}
-                </ContainerBay>
-              </div>
-            ))}
+
+      <div className="flex flex-col">
+        <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+          {/* Simulation Area */}
+          <div className="p-5" style={{ height: "100%", backgroundColor: "#f0f0f0", overflowX: "auto" }}>
+            <div className="flex" style={{ width: "max-content" }}>
+              {Array.from({ length: bayCount }).map((_, bayIndex) => (
+                <div key={`bay-${bayIndex}`}>
+                  <h5 className="text-center text-md font-medium mb-2">Bay {bayIndex + 1}</h5>
+                  <ContainerBay id={`bay-${bayIndex}`} rows={baySize.rows} columns={baySize.columns}>
+                    {Array.from({ length: baySize.rows * baySize.columns }).map((_, cellIndex) => {
+                      const rowIndex = Math.floor(cellIndex / baySize.columns);
+                      const colIndex = cellIndex % baySize.columns;
+                      const coordinates = `${bayIndex + 1}${rowIndex}${colIndex}`;
+                      return (
+                        <DroppableCell key={`bay-${bayIndex}-${cellIndex}`} id={`bay-${bayIndex}-${cellIndex}`} coordinates={coordinates}>
+                          {droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`) && (
+                            <DraggableContainer
+                              id={droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`).id}
+                              text={droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`).id}
+                              isDragging={draggingItem === droppedItems.find((item) => item.area === `bay-${bayIndex}-${cellIndex}`).id}
+                            />
+                          )}
+                        </DroppableCell>
+                      );
+                    })}
+                  </ContainerBay>
+                </div>
+              ))}
+            </div>
           </div>
-          <h4 className="text-lg font-semibold mt-8 mb-4">Docks</h4>
-          <div className="flex flex-col items-start">
+
+          {/* Docks Area */}
+          <div className="flex flex-col items-center" style={{ height: "100%", width: "100%", backgroundColor: "#e0e0e0" }}>
             <ContainerDocks id="docks">
-              {Array.from({ length: 15 }).map((_, index) => (
+              {paginatedItems.map((item, index) => (
                 <DroppableCell key={`docks-${index}`} id={`docks-${index}`}>
-                  {droppedItems.find((item) => item.area === `docks-${index}`) && (
-                    <DraggableContainer id={droppedItems.find((item) => item.area === `docks-${index}`).id} text={droppedItems.find((item) => item.area === `docks-${index}`).id} />
-                  )}
+                  {item.area.startsWith("docks") && <DraggableContainer id={item.id} text={item.id} isDragging={draggingItem === item.id} />}
                 </DroppableCell>
               ))}
             </ContainerDocks>
+            <div className="flex justify-center gap-2 w-full mt-4">
+              <button onClick={() => handlePageChange(-1)} disabled={currentPage === 0} className="px-4 py-2 bg-gray-300 rounded">
+                Previous
+              </button>
+              <button onClick={() => handlePageChange(1)} disabled={(currentPage + 1) * itemsPerPage >= droppedItems.length} className="px-4 py-2 bg-gray-300 rounded">
+                Next
+              </button>
+            </div>
           </div>
-        </div>
-      </DndContext>
+
+          {/* DragOverlay */}
+          <DragOverlay>{draggingItem ? <DraggableContainer id={draggingItem} text={draggingItem} style={{ zIndex: 9999 }} /> : null}</DragOverlay>
+        </DndContext>
+      </div>
     </>
   );
 };
