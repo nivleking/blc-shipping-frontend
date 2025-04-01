301b0654beb08be9ab3f5f6b384a490988f01d62
Kelvin Sidharta Sie <c14210307@john.petra.ac.id>
2025-04-01 18:22:25 +0700

    feat: change stale time to 5 minutes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import AppProvider from "./context/AppContext.jsx";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 5 * 60 * 1000,
       cacheTime: 5 * 60 * 1000,
       refetchOnWindowFocus: false,
       refetchOnMount: true,
