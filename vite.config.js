09bff375fce2394bce2ad3973574e9ccdfe45ea4
nivleking <kelvinsidhartasie@gmail.com>
2026-01-06 22:00:11 +0700

    fix(vite): update allowedHosts in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
+    // base: '/blc-shipping/',
     plugins: [react()],
     server: {
       host: true,
@@ -22,6 +22,8 @@ export default defineConfig(({ mode }) => {
       port: env.VITE_SERVER_PORT,
       strictPort: true,
       allowedHosts: [
+        "blc.petra.ac.id/*",
+        "blc.petra.ac.id",
         "kelvinsidhartasie.my.id",
         "*.kelvinsidhartasie.my.id",
         "frontend.kelvinsidhartasie.my.id",
@@ -29,6 +31,7 @@ export default defineConfig(({ mode }) => {
         "slg.petra.ac.id",
         "*.blc.works",
         "blc.works",
+        "192.168.160.20"
       ],
     },
   };
